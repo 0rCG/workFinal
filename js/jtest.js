223bd3706a480114e7fcd1e810e727915421ade6
0rCG <13_0rC@mail.ru>
2018-10-22 21:46:37 +0300

    first commit of final project

---
+++ js/jtest.js
@@ -0,0 +1,51 @@
+$('.minus-btn').on ('click', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest('div').find('input');
+    var value = parseInt($input.val());
+ 
+    if (value > 1) {
+        value = value - 1;
+    } else {
+        value = 0;
+    }
+ 
+  $input.val(value);
+ 
+});
+ 
+$('.plus-btn').on('click', function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $input = $this.closest('div').find('input');
+    var value = parseInt($input.val());
+ 
+    if (value  < 100) {
+        value = value + 1;
+    } else {
+        value =100;
+    }
+ 
+    $input.val(value);
+});
+
+
+$(document).ready(function() {
+    $('.deleteRow').click(function() {
+        $(this).parent().parent().remove();
+    })
+
+    $('#reset').click(function() {
+        $('.shopping-cart').remove();
+    })
+});
+
+var hotbod = document.querySelector(".animate_title");
+
+function doStuff() {
+    hotbod.className += " animate";
+}
+
+window.onload = function() {
+    doStuff();
+};
